1ea18b06293e9463f9958ff6a0cb91591e37015f
Hasekawa Mikan <140880794+ZundaDD@users.noreply.github.com>
2025-02-26 11:46:33 +0800

    Modify Judge Feedback

--- Assets/Script/UI/AnimeUI/FlashText.cs
+++ Assets/Script/UI/AnimeUI/FlashText.cs
@@ -6,19 +6,30 @@ namespace Megaton.UI
 {
     public class FlashText : MonoBehaviour
     {
-        private static float exist_time = 0.6f;
+        private static float exist_time = 0.8f;
+        private static float x_scale = 10f;
+        private static float y_scale = 15f;
+        [SerializeField] private AnimationCurve xCurve;
+        [SerializeField] private AnimationCurve yCurve;
+        private RectTransform rectT;
+        private Vector2 origin;
         private float timer = 0;
         private Text text;
 
         void Start()
         {
+            rectT = GetComponent<RectTransform>();
             text = GetComponent<Text>();
-            text.DOFade(0, exist_time);
+            origin = rectT.anchoredPosition;
+            text.DOFade(0, exist_time).SetEase(Ease.InQuad);
         }
 
         void Update()
         {
             timer += Time.deltaTime;
+            rectT.anchoredPosition = origin +
+                new Vector2(xCurve.Evaluate(timer / exist_time) * x_scale,
+                yCurve.Evaluate(timer / exist_time) * y_scale);
             if (timer > exist_time) Destroy(gameObject);
         }
     }
